db2a48c097a6acc08b3ac6955ed1f818efb5bf98
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-28 12:23:44 +0000

    [Finish]bookers2-7c

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   get 'chats/:id', to: 'chats#show', as: 'chat'
   resources :chats, only: [:create]
 
+  resources :groups, except: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
